348c8a15053bc30e33725c7a5d39939ea5ea33e2
mahmednabil109 <mahmednabil109@gmail.com>
2022-11-19 21:20:29 +0200

    Configure Junit
    - add scratch file
    - configure jdk to 15
    
    Signed-off-by: mahmednabil109 <mahmednabil109@gmail.com>

---
+++ src/code/CoastGuard.java
@@ -0,0 +1,12 @@
+package code;
+
+public class CoastGuard {
+
+    public static String solve(String problem, String alog, boolean visualize){
+        return "";
+    }
+
+    public static String GenGrid(){
+        return "";
+    }
+}
